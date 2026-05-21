1c96181c33fd12e6b76cd4ededaf328c44643904
Long Nguyen <heinsburg2005@gmail.com>
2026-05-20 23:37:54 -0400

    minor adjustments

--- src/jags_pangolin/engine.py
+++ src/jags_pangolin/engine.py
@@ -10,22 +10,38 @@ import tempfile
 from pathlib import Path
 
 
-def ensure_size( arr, sizes, depth=0):
+def ensure_size(arr, sizes, depth=0):
     while len(arr) < sizes[depth]:
         if depth == len(sizes) - 1:
-            arr.append(None)   
+            arr.append(None)
         else:
             arr.append([])
     return arr
+
+
+def write_constant(f, name, val):
+    """Write a constant to a JAGS data.R file with full float64 precision (:.17g)."""
+    arr = np.array(val, dtype=np.float64)
+    if arr.ndim == 0:
+        f.write(f"{name} <- {float(arr):.17g}\n")
+    elif arr.ndim == 1:
+        vals_str = ", ".join(f"{v:.17g}" for v in arr)
+        f.write(f"{name} <- c({vals_str})\n")
+    else:
+        # JAGS is column-major, so flatten with order='F'
+        flat = arr.flatten(order='F')
+        vals_str = ", ".join(f"{v:.17g}" for v in flat)
+        dims = ", ".join(str(d) for d in arr.shape)
+        f.write(f"{name} <- structure(c({vals_str}), .Dim = c({dims}))\n")
 
 
 class Sample_prob:
     class RunDFS:
         def __init__(self):
             self.visited = {}
-        
+
         def dfs(self, node):
-            name = "v"+str(node._n)
+            name = "v" + str(node._n)
             if name in self.visited:
                 return
             self.visited[name] = node
@@ -35,11 +51,9 @@ class Sample_prob:
         def run_dfs(self, nodes):
             for node in nodes:
                 self.dfs(nodes[node])
-            return self.visited 
+            return self.visited
 
-    def sample(self, sample_vars:list[RV], kwargs=[], values = [], niter=1000, debug = False):
-        # for var in sample_vars:
-        #     print(repr(var))
+    def sample(self, sample_vars: list[RV], kwargs=[], values=[], niter=1000, debug=False):
         with tempfile.TemporaryDirectory() as tmpdir:
             tmp = Path(tmpdir)
             data_path = tmp / "data.R"
@@ -50,75 +64,69 @@ class Sample_prob:
 
             dic = {}
             for var in kwargs:
-                dic["v"+str(var._n)] = var
+                dic["v" + str(var._n)] = var
             for sample_var in sample_vars:
-                dic["v"+str(sample_var._n)] = sample_var 
+                dic["v" + str(sample_var._n)] = sample_var
             app = self.RunDFS()
             res = app.run_dfs(dic)
 
+            # Write data.R with full float64 precision via write_constant()
+            # Previously used Scalar_ops.Constant_before which truncated to ~6 sig figs
             with open(data_path, "w") as f:
                 for node in res:
-                    if(res[node].op.name == "Constant"):
-                        f.write(Scalar_ops.__dict__["Constant_before"](node, res[node]))
+                    if res[node].op.name == "Constant":
+                        write_constant(f, node, res[node].op.value)
                 for i in range(len(kwargs)):
-                    f.write(Scalar_ops.__dict__["Constant_before"](f"v{kwargs[i]._n}", RV(Constant(values[i]))))
-                f.close()
+                    write_constant(f, f"v{kwargs[i]._n}", values[i])
 
-            with open(data_path, "r") as f:
-                data_code = f.read()
-                # print(data_code)
-                if(debug):
-                    print(data_code)
-                f.close()
-            
-            with open( model_path, "w") as f:
+            if debug:
+                with open(data_path, "r") as f:
+                    print(f.read())
+
+            with open(model_path, "w") as f:
                 f.write("model {\n")
-                check = {}  
-                for node in sorted(res): 
+                check = {}
+                for node in sorted(res):
                     if node in check:
                         continue
                     check[node] = True
                     parents = [f"v{res[node].parents[i]._n}" for i in range(len(res[node].parents))]
-                    if(flow.__dict__.get(res[node].op.name) is not None):
+                    if flow.__dict__.get(res[node].op.name) is not None:
                         tmp_p = [res[node].parents[i].shape for i in range(len(res[node].parents))]
-                        code = flow.__dict__[res[node].op.name](node, res[node].op, parents,0, tmp_p)
+                        code = flow.__dict__[res[node].op.name](node, res[node].op, parents, 0, tmp_p)
                         f.write(code + "\n")
-                    elif(index.__dict__.get(res[node].op.name) is not None):
+                    elif index.__dict__.get(res[node].op.name) is not None:
                         tmp_p = index()
                         tmpp = [res[node].parents[i] for i in range(len(res[node].parents))]
                         code = tmp_p.Index(node, parents, tmpp)
                         f.write(code + "\n")
-                    elif(res[node].op.name!="Constant" and Scalar_ops.__dict__.get(res[node].op.name) is not None):
+                    elif res[node].op.name != "Constant" and Scalar_ops.__dict__.get(res[node].op.name) is not None:
                         code = Scalar_ops.__dict__[res[node].op.name](node, parents)
                         f.write(code + "\n")
-                    elif(Multi_funcs.__dict__.get(res[node].op.name) is not None):
+                    elif Multi_funcs.__dict__.get(res[node].op.name) is not None:
                         tmp_p = [res[node].parents[i].shape for i in range(len(res[node].parents))]
                         code = Multi_funcs.__dict__[res[node].op.name](node, res[node].op, parents, tmp_p)
                         f.write(code + "\n")
-                f.write("}\n") 
-                f.close()
-            
-            with open(model_path, "r") as f:
-                model_code = f.read()
-                # print(model_code)
-                if(debug):
-                    print(model_code)
-                f.close()
+                f.write("}\n")
+
+            if debug:
+                with open(model_path, "r") as f:
+                    print(f.read())
 
             with open(script_path, "w") as f:
-                # Use absolute paths so JAGS knows where files are
                 script = f'model in "{str(model_path.absolute()).replace(chr(92), "/")}"\n'
                 script += f'data in "{str(data_path.absolute()).replace(chr(92), "/")}"\n'
                 script += "compile, nchains(1)\n"
                 script += "initialize\n"
                 script += "update 1000\n"
                 for sample_var in sample_vars:
-                    script += f"monitor {('v'+str(sample_var._n))}\n"
+                    script += f"monitor {'v' + str(sample_var._n)}\n"
                 script += f"update {niter}\n"
-                script += f'coda *\n'
+                script += "coda *\n"
                 f.write(script)
-                if(debug):
+                if debug:
                     print(script)
+
             system = platform.system()
             if system == "Windows":
                 jags_path = "C:/Program Files/JAGS/JAGS-4.3.2/x64/bin/jags.bat"
@@ -126,13 +134,13 @@ class Sample_prob:
                 subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).decode()
             else:  # Linux/macOS
                 cmd = ['jags', 'script.txt']
-                subprocess.check_output(cmd, stderr=subprocess.STDOUT, cwd = tmp).decode()
+                subprocess.check_output(cmd, stderr=subprocess.STDOUT, cwd=tmp).decode()
+
             return self.read_coda(sample_vars, coda_chain, coda_index)
 
     def read_coda(self, sample_vars, coda_chain, coda_index):
         result = {}
-        
-        # Load MCMC samples
+
         with open(coda_chain, "r") as f:
             res_lines = [float(line.strip().split()[1]) for line in f]
 
@@ -146,7 +154,7 @@ class Sample_prob:
                 name = match.group(1)
                 index_str = match.group(2)
 
-                values = res_lines[int(start)-1 : int(end)]
+                values = res_lines[int(start) - 1: int(end)]
 
                 if index_str is None:
                     result[name] = values
@@ -158,24 +166,20 @@ class Sample_prob:
                     result[name] = []
 
                 arr = result[name]
-
                 arr = ensure_size(arr, indices)
 
                 ref = arr
-                for d in range(len(indices)-1):
+                for d in range(len(indices) - 1):
                     idx = indices[d] - 1
-                    ref[idx] = ensure_size(ref[idx], indices, depth=d+1)
+                    ref[idx] = ensure_size(ref[idx], indices, depth=d + 1)
                     ref = ref[idx]
 
                 ref[indices[-1] - 1] = values
-
                 result[name] = arr
+
         final = []
         for var in sample_vars:
             final.append(result[f"v{var._n}"])
         for i in range(len(final)):
             final[i] = np.moveaxis(np.array(final[i]), -1, 0)
-        # print(final[0].shape)
         return final
-
-
